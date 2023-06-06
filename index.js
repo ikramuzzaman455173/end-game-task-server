7f212a516d03c47cbdd4d0fe0500e895c4bcb006
jakaria455173 <jakaria455173@gmail.com>
2023-06-06 14:38:47 +0600

    many thinks changed or added

--- index.js
+++ index.js
@@ -1,10 +1,70 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+// const stripe = require("stripe")(process.env.payment_secreat_key);
+// const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000
 const cors = require('cors');
 
 app.use(cors())
 app.use(express.json())
+
+//varify jwt
+const varifyJwt = (req, res, next) => {
+  const authorization = req.headers.authorization
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
+  }
+  //bearer token
+  const token = authorization.split(' ')[1]
+  jwt.verify(token, process.env.access_token_secreat_key, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded
+    next()
+  })
+}
+
+const uri = `mongodb+srv://${process.env.dbuser}:${process.env.dbPass}@cluster0.izhktyr.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
 
 app.get('/', (req, res) => {
   res.send('<h1 style="color:#333;text-align:center;font-size:20px;margin:10px 0;">Coffee Store Server Is Running !!!</h1>')
