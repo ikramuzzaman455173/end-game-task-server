776d9b8486fa9f59e5710110f0fd8d60fa40058b
jakaria455173 <jakaria455173@gmail.com>
2023-07-24 08:46:46 +0600

    commit

--- index.js
+++ index.js
@@ -4,30 +4,15 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 // const stripe = require("stripe")(process.env.payment_secreat_key);
 // const jwt = require('jsonwebtoken');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000
 const cors = require('cors');
 
 app.use(cors())
 app.use(express.json())
 
-//varify jwt
-const varifyJwt = (req, res, next) => {
-  const authorization = req.headers.authorization
-  if (!authorization) {
-    return res.status(401).send({ error: true, message: 'unauthorized access' })
-  }
-  //bearer token
-  const token = authorization.split(' ')[1]
-  jwt.verify(token, process.env.access_token_secreat_key, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ error: true, message: 'unauthorized access' })
-    }
-    req.decoded = decoded
-    next()
-  })
-}
 
-const uri = `mongodb+srv://${process.env.dbuser}:${process.env.dbPass}@cluster0.izhktyr.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Key}@cluster0.wg8qvkk.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -41,69 +26,53 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    const usersCollection = client.db('simpleDb').collection('users')
+    // await client.connect();
+    const collegeCollection = client.db("Job_Task_College_Boking").collection("colleges")
+    const userCollection = client.db("Job_Task_College_Boking").collection("users")
+    const reviewCollection = client.db("Job_Task_College_Boking").collection("reviews")
 
-    //post jwt
-    app.post('/jwt', (req, res) => {
-      const user = req.body
-      // console.log('user',user);
-      const token = jwt.sign(user, process.env.access_token_secreat_key, { expiresIn: '1h' })
-      res.send({ token })
+
+    app.get("/colleges", async (req, res) => {
+        const result = await collegeCollection.find().toArray();
+        res.send(result)
     })
 
-    // varifyAdminJwt
-    // const varifyAdminJwt = async (req, res, next) => {
-    //   const email = req.decoded.email
-    //   const query = { email: email }
-    //   const user = await usersCollection.findOne(query)
-    //   if (user?.role !== 'admin') {
-    //     return res.status(403).send({error:true,message:'forbidden message'})
-    //   }
-    //   next()
-    // }
+    app.post("/users", async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existingUsers = await userCollection.findOne(query)
+        if (existingUsers) {
+            return res.send({ message: "user already exiists" })
+        }
+        const result = await userCollection.insertOne(user)
+        res.send(result)//i
+    })
 
+    app.get('/college/:id', async (req, res) => {
+        const collegeId = req.params.id;
+        const query = { _id: new ObjectId(collegeId) }
+        const result = await collegeCollection.findOne(query);
+        res.send(result)
+    })
 
+    app.get("/reviews", async (req, res) => {
+        const result = await reviewCollection.find().toArray();
+        res.send(result)
+    })
 
-
-
-
-
-
-
-
-
-
-      // create payments intent
-      // app.post('/payment',varifyJwt,async (req,res) => {
-      //   const { price } = req.body
-      //   const amount = parseInt(price * 100)
-      //   console.log('price',price,'amount',amount);
-      //   const paymentIntent = await stripe.paymentIntents.create({
-      //     amount: amount,
-      //     currency: 'usd',
-      //     payment_method_types:['card']
-      //   })
-      //   res.send({
-      //     clientSecret:paymentIntent.client_secret
-      //   })
-      // })
-
-
-
-
-
-
-
-
+    app.post("/review", async (req, res) => {
+        const item = req.body
+        const result = await reviewCollection.insertOne(item)
+        res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+} finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
-  }
+}
 }
 run().catch(console.dir);
 
