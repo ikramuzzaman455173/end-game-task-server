1697f62e43ba762b2447d567fe28d49e37590901
jakaria455173 <jakaria455173@gmail.com>
2023-07-24 14:19:49 +0600

    commit

--- index.js
+++ index.js
@@ -1,94 +1,60 @@
 const express = require('express');
+const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
-
 require('dotenv').config();
-
-const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_Key}@cluster0.wg8qvkk.mongodb.net/?retryWrites=true&w=majority`;
 
+const uri = `mongodb+srv://${process.env.dbuser}:${process.env.dbPass}@cluster0.izhktyr.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  },
+  }
 });
 
-async function connectToMongoDB() {
+async function run() {
   try {
-    // await client.connect();
+    // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      const usersCollection = client.db('summerCampSchool').collection('users')
+      app.get('/users', async (req, res) => {
+        const users = await usersCollection.find({}).toArray()
+        res.send(users)
+      })
 
-    const collegeCollection = client.db('Job_Task_College_Boking').collection('colleges');
-    const userCollection = client.db('Job_Task_College_Boking').collection('users');
-    const reviewCollection = client.db('Job_Task_College_Boking').collection('reviews');
-
-    app.get('/colleges', async (req, res) => {
-      try {
-        const result = await collegeCollection.find().toArray();
-        res.send(result);
-      } catch (error) {
-        console.error('Error fetching colleges:', error);
-        res.status(500).send('Internal Server Error');
-      }
-    });
-
-    // Other routes...
-
-    app.get("/colleges", async (req, res) => {
-        const result = await collegeCollection.find().toArray();
-        res.send(result)
-    })
-
-    app.get('/college/:id', async (req, res) => {
-        const collegeId = req.params.id;
-        const query = { _id: new ObjectId(collegeId) }
-        const result = await collegeCollection.findOne(query);
-        res.send(result)
-    })
-
-    app.get("/reviews", async (req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result)
-    })
-
-    app.post("/users", async (req, res) => {
-        const user = req.body;
-        const query = { email: user.email }
-        const existingUsers = await userCollection.findOne(query)
-        if (existingUsers) {
-            return res.send({ message: "user already exiists" })
-        }
-        const result = await userCollection.insertOne(user)
-        res.send(result)//i
-    })
-
-    app.post("/review", async (req, res) => {
-        const item = req.body
-        const result = await reviewCollection.insertOne(item)
-        res.send(result)
-    })
+      // database users data hanlde api
+      app.get('/allUsers', async (req, res) => {
+        const users = await usersCollection.find({}).toArray()
+        res.send(users)
+      })
 
 
 
 
-    console.log('Pinged your deployment. You successfully connected to MongoDB!');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
   }
 }
-
-connectToMongoDB();
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('running');
-});
+    res.send('<h1 style="color:#333;text-align:center;font-size:20px;margin:10px 0;">Summer Camp School Server Is Running !!!</h1>')
+})
 
 app.listen(port, () => {
-  console.log(`Running on port: ${port}`);
-});
+    console.log(`End Game Task Server Is Running On Port:http://localhost:${port}`);
+})
